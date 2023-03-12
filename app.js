bde944b7f8c8966fedb4ba888991810c90e43c43
joessir <lassiryyass@gmail.com>
2023-03-12 19:29:52 +0100

    playing with drag and drop

--- app.js
+++ app.js
@@ -8,33 +8,71 @@ class UI {
   static deleteWall(cell) {
     if (cell.classList.contains('wall')) cell.classList.remove('wall');
   }
-
 }
+
 
 (function entryPoint() {
   let mouseDown = false;
   let btn = 0;
   const container = document.getElementById('grid-container');
+  const draggable = document.querySelector('.item');
+  const cells = container.childNodes;
+
+  draggable.addEventListener('dragstart', (e) => {
+    e.dataTransfer.setData('text/plain', e.target.id);
+  });
 
 
-  container.addEventListener('contextmenu', (e) => {
+
+  const dragEnter = (e) => {
     e.preventDefault();
+  };
+
+  const dragOver = (e) => {
+    e.preventDefault();
+    e.target.classList.add('drag-over');
+  };
+
+  const dragLeave = (e) => {
+    e.target.classList.remove('drag-over');
+  };
+
+  const drop = (e) => {
+    console.log(4);
+    e.target.classList.remove('drag-over');
+    e.target.classList.add("start");
+    document.body.removeChild(draggable);
+  };
+
+
+  container.addEventListener('dragenter', dragEnter);
+  container.addEventListener('dragover', dragOver);
+  container.addEventListener('dragleave', dragLeave);
+  container.addEventListener('drop', drop);
+
+
+  container.addEventListener('keydown', (e) => {
+    console.log(e);
   });
 
-  container.addEventListener('mousedown', (e) => {
-    mouseDown = true;
-    btn = e.button;
-  });
-  container.addEventListener('mousemove', (e) => {
-    if (mouseDown) {
-      btn === 0
-        ? UI.addWall(e.target)
-        : UI.deleteWall(e.target);
-    }
-  });
-  container.addEventListener('mouseup', () => {
-    mouseDown = false;
-  });
+  //   container.addEventListener('contextmenu', (e) => {
+  //     e.preventDefault();
+  //   });
+
+  //   container.addEventListener('mousedown', (e) => {
+  //     mouseDown = true;
+  //     btn = e.button;
+  //   });
+  //   container.addEventListener('mousemove', (e) => {
+  //     if (mouseDown) {
+  //       btn === 0
+  //         ? UI.addWall(e.target)
+  //         : UI.deleteWall(e.target);
+  //     }
+  //   });
+  //   container.addEventListener('mouseup', () => {
+  //     mouseDown = false;
+  //   });
 
   const drawBoard = () => {
     const rows = 10;
