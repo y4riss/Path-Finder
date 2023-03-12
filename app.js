d226f05b13923d72ac06bff02159e4779b03ebe7
joessir <lassiryyass@gmail.com>
2023-03-12 21:16:45 +0100

    implementing breadth first search algorithm ..

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let grid = [];
+let startPos = {x : 0, y : 0}
+let destPos = {x : 0, y : 0}
 class UI {
   static addWall(cell) {
     if (cell.classList.contains('cell') && !cell.classList.contains('wall')) {
@@ -28,6 +31,18 @@ class UI {
     const drop = (e) => {
       if (!e.target.classList.contains('wall')) {
         e.target.classList.add(className);
+        const cellIndex = e.target.dataset.index;
+        if (className === 'start')
+        {
+            startPos.x = Math.floor(cellIndex / grid.length)
+            startPos.y =  cellIndex % grid[0].length
+            grid[startPos.x][startPos.y] = 2;
+        }
+
+        else
+        destPos.x = Math.floor(cellIndex / grid.length)
+        destPos.y =  cellIndex % grid[0].length
+        grid[destPos.x][destPos.y] = 3;
       }
       container.removeEventListener('dragenter', dragEnter);
       container.removeEventListener('dragover', dragOver);
@@ -42,7 +57,39 @@ class UI {
   }
 }
 
-const solver = () => {};
+const solver = (grid,startPos,destPos) => {
+    
+    const queue = [startPos]
+
+    // creating visited array and filling it with 0
+    const visited = []
+    for(let i = 0 ; i < grid.length ; i++)
+    {
+        const visitedRow = []
+        for(let j = 0 ; j < grid[0].length ; j++)
+        visitedRow.push(0)
+        visited.push(visitedRow)
+    }
+
+    while (queue.length)
+    {
+        const elem = queue.shift(); // dequeue node from the queue
+        visited[elem.x][elem.y] = 1; // mark the node as visited
+
+        // visiting all its neighbours
+        /*
+        [0,0,0,0]
+        [0,0,0,0]
+        [0,0,0,0]
+        [0,0,0,0]
+        */
+       if (elem.x != 0 && elem.x != grid.length - 1 && elem.y != 0 && elem.y != grid[0].length)
+       {
+        
+       }
+
+    }
+};
 
 (function entryPoint() {
   let mouseDown = false;
@@ -53,7 +100,6 @@ const solver = () => {};
   const startDiv = document.querySelector('.start-div');
   const endDiv = document.querySelector('.end-div');
   const solveBtn = document.querySelector('.solve');
-  let grid = [];
   startDiv.addEventListener('dragstart', () => {
     UI.dragDrop(container, 'start');
   });
@@ -76,12 +122,10 @@ const solver = () => {};
         UI.addWall(e.target);
         const cellIndex = e.target.dataset.index;
         grid[Math.floor(cellIndex / rows)][cellIndex % cols] = 1;
-
       } else {
         UI.deleteWall(e.target);
         const cellIndex = e.target.dataset.index;
         grid[Math.floor(cellIndex / rows)][cellIndex % cols] = 0;
-
       }
     }
   });
@@ -89,8 +133,8 @@ const solver = () => {};
     mouseDown = false;
   });
 
-  solveBtn.addEventListener('click', (e) => {
-    console.log(grid);
+  solveBtn.addEventListener('click', () => {
+    solver(grid,startPos,destPos);
   });
   const drawBoard = () => {
     rows = 10;
